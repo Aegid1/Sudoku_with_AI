02ed6e5db8dd21a4257c0688ddf269a4490ac2b5
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-02-22 18:34:02 +0100

    depth-first-search algorithmus implementiert

--- function.py
+++ function.py
@@ -109,9 +109,54 @@ def eliminate(values):
 #for i in trial:
 #    trial[i] = ''.join(sorted(trial[i]))
 
-def main():
 
+def search(values):
+    '''
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    '''
+    # First, reduce the puzzle using the previous function
+    completed = False
+    previous_results = []
+    i = 0
+    item_with_min = 'A1'
     
+    while completed == False:
+        
+        values = eliminate(values)
+        values = only_choice(values)
+        
+        resulting_puzzle = len([value for value in values if len(values[value]) == 1])
+        previous_results.append(resulting_puzzle)
+        i += 1
+        
+        if resulting_puzzle == len(values):
+            completed = True
+            return values
+            
+        
+        if len(previous_results) > 1 and previous_results[i] == previous_results[i-1]:
+            return False
+        
+
+        for item in values:
+                
+            if len(values[item]) <= len(values[item_with_min]):
+                    item_with_min = item
+                    
+        for digit in values[item_with_min]: 
+                
+            values_copy = values.copy()
+            values_copy[item_with_min] = digit
+            result = search(values_copy)
+            
+            if result:
+                return result
+            
+    return False
+
+
+
+def main():
 
     grid = {'A1': '123456789', 'A2': '123456789', 'A3': '3', 'A4': '123456789', 'A5': '2', 'A6': '123456789', 'A7': '6', 'A8': '123456789', 'A9': '123456789', 'B1': '9', 'B2': '123456789', 'B3': '123456789', 'B4': '3', 'B5': '123456789', 'B6': '5', 'B7': '123456789', 'B8': '123456789', 'B9': '1', 'C1': '123456789', 'C2': '123456789', 'C3': '1', 'C4': '8', 'C5': '123456789', 'C6': '6', 'C7': '4', 'C8': '123456789', 'C9': '123456789', 'D1': '123456789', 'D2': '123456789', 'D3': '8', 'D4': '1', 'D5': '123456789', 'D6': '2', 'D7': '9', 'D8': '123456789', 'D9': '123456789', 'E1': '7', 'E2': '123456789', 'E3': '123456789', 'E4': '123456789', 'E5': '123456789', 'E6': '123456789', 'E7': '123456789', 'E8': '123456789', 'E9': '8', 'F1': '123456789', 'F2': '123456789', 'F3': '6', 'F4': '7', 'F5': '123456789', 'F6': '8', 'F7': '2', 'F8': '123456789', 'F9': '123456789', 'G1': '123456789', 'G2': '123456789', 'G3': '2', 'G4': '6', 'G5': '123456789', 'G6': '9', 'G7': '5', 'G8': '123456789', 'G9': '123456789', 'H1': '8', 'H2': '123456789', 'H3': '123456789', 'H4': '2', 'H5': '123456789', 'H6': '3', 'H7': '123456789', 'H8': '123456789', 'H9': '9', 'I1': '123456789', 'I2': '123456789', 'I3': '5', 'I4': '123456789', 'I5': '1', 'I6': '123456789', 'I7': '3', 'I8': '123456789', 'I9': '123456789'}
     eliminate(grid)
